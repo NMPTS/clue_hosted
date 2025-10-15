dbe1f827ee984db04e15192861b2e90e75145efb
Srinath-exe <1406srinath@gmail.com>
2025-10-15 16:46:43 +0530

    Deploy Version 1.0.0+1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1627334067"
+    serviceWorkerVersion: "1807862456"
   }
 });
